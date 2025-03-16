1a0756293dd42f1afc7c35756511285b6ed69e10
lurenfu <lurenfu@jiuqi.com.cn>
2025-03-16 16:51:00 +0800

    sentinel 流控测试

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/lu/cloud/controller/FlowLimitController.java
@@ -0,0 +1,37 @@
+package com.lu.cloud.controller;
+
+import com.lu.cloud.service.FlowLimitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlowLimitController {
+
+    @Autowired
+    private FlowLimitService flowLimitService;
+
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "------testB";
+    }
+
+    @GetMapping("/testC")
+    public String testC() {
+
+        flowLimitService.common();
+        return "------testC";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        flowLimitService.common();
+        return "------testD";
+    }
+
+}
